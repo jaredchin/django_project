66cb6b55e03e4d226373e1cfa8476d6b859c0a03
jaredchin <1007707989@qq.com>
2018-04-09 17:40:48 +0800

    Initial commit

---
+++ myfirstsite/article/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from .models import Article
+# Create your views here.
+
+def article_detail(request, article_id):
+        article = get_object_or_404(Article, pk=article_id)
+        context = {}
+        context['article_obj'] = article
+        #return render(request, "article_detail.html", context)
+        return render_to_response("article_detail.html", context)
+
+def article_list(request):
+    articles = Article.objects.filter(is_deleted=False)
+    context = {}
+    context['articles'] = articles
+    return render_to_response('article_list.html', context)
