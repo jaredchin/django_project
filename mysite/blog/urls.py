dc1f966de8c336e89fdb6ed0387aa7ecc979c52c
jaredchin <1007707989@qq.com>
2018-04-10 22:35:55 +0800

    1.0

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:blog_pk>', views.blog_detail, name='blog_detail'),
+    path('type/<int:blog_type_pk>', views.blogs_with_type, name='blogs_with_type')
 ]
