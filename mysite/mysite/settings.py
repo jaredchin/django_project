2fa17a2f5c7ee5529bec04dd766213977a3e9a29
jaredchin <1007707989@qq.com>
2018-04-18 19:47:33 +0800

    1.0

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -137,3 +137,11 @@ CKEDITOR_UPLOAD_PATH = 'upload/'
 
 # 自定义参数
 EACH_PAGE_BLOG_NUMBERS = 4
+
+# 缓存数据
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'my_cache_table',
+    }
+}
